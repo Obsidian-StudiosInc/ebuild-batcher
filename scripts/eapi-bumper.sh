3dc848b70a90014582c7eaeb2eef896927712012
William L. Thomson Jr <wlt@o-sinc.com>
2016-11-07 13:56:17 -0500

    scripts/eapi-bumper.sh: Initial batch script to bump EAPI on ebuilds

---
+++ scripts/eapi-bumper.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Copyright 2016 Obsidian-Studios, Inc.
+# Author William L. Thomson Jr.
+#        wlt@o-sinc.com
+#
+# Distributed under the terms of The GNU Public License v3.0 (GPLv3)
+
+# Batch script file to update eapi
+
+EAPI="6"
+COMMIT_MSG="Bumped to EAPI=${EAPI}"
+TREE="/usr/portage/local/os-xtoo"
+
+cd ${TREE} || exit 1
+
+PKGS=($( grep -ls 'EAPI="\?5"\?' */*/*ebuild ))
+
+batch_cmds() {
+	echo "Updating EAPI -> ${ebuild}"
+	sed -i -e "s|EAPI=\"\?[0-9]\"\?|EAPI=\"${EAPI}\"|" ${ebuild} \
+		|| return 1
+	return 0
+}
