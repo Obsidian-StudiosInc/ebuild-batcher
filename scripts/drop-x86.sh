25f0ed958d727a810361f4c5b2c666d5a7b7f6c1
William L. Thomson Jr <wlt@o-sinc.com>
2016-11-11 20:33:16 -0500

    scripts/drop-x86.sh: Script to drop x86 keyword

---
+++ scripts/drop-x86.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Copyright 2016 Obsidian-Studios, Inc.
+# Author William L. Thomson Jr.
+#        wlt@o-sinc.com
+#
+# Distributed under the terms of The GNU Public License v3.0 (GPLv3)
+
+# Batch script file to drop x86 keyword
+
+COMMIT_MSG="Dropped keyword x86"
+TREE="/usr/portage/local/os-xtoo"
+
+cd ${TREE} || exit 1
+
+PKGS=($( grep -ls x86 */*/*ebuild ))
+
+batch_cmds() {
+	echo "${COMMIT_MSG} -> ${ebuild}"
+	sed -i -e "s| ~x86||" ${ebuild} || return 1
+	return 0
+}
