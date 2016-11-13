ac8d23f2b8882684c74f6965aa762d0b816adcd7
William L. Thomson Jr <wlt@o-sinc.com>
2016-11-13 12:20:01 -0500

    scripts/rm-java_prepare-and-java-pkg_clean.sh: Batch script file
    Batch script file to remove java_prepare and java-pkg_clean

---
+++ scripts/rm-java_prepare-and-java-pkg_clean.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Copyright 2016 Obsidian-Studios, Inc.
+# Author William L. Thomson Jr.
+#        wlt@o-sinc.com
+#
+# Distributed under the terms of The GNU Public License v3.0 (GPLv3)
+
+# Batch script file to remove java_prepare and java-pkg_clean
+
+COMMIT_MSG="Removed java_prepare & java-pkg_clean"
+TREE="/usr/portage/local/os-xtoo"
+
+cd ${TREE} || exit 1
+
+PKGS=($( grep -ls "java-pkg_clean" */*/*ebuild ))
+
+batch_cmds() {
+	echo "Removing java_prepare & java-pkg_clean -> ${ebuild}"
+	sed -i '/^$/ {N;N;N; /java_prepare() {.*java-pkg_clean/d}' \
+		${ebuild} || return 1
+	return 0
+}
