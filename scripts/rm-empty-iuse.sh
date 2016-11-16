73ce78ba0c1b04fe5c3b9439e5d2d64ccd0d1451
William L. Thomson Jr <wlt@o-sinc.com>
2016-11-16 10:19:58 -0500

    scripts/rm-empty-iuse.sh: Initial script to remove empty IUSE
    Script could be improved to remove leading/trailing empty lines if they
    exist. Otherwise you end up with a extra empty line that repoman
    complains about.

---
+++ scripts/rm-empty-iuse.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Copyright 2016 Obsidian-Studios, Inc.
+# Author William L. Thomson Jr.
+#        wlt@o-sinc.com
+#
+# Distributed under the terms of The GNU Public License v3.0 (GPLv3)
+
+# Batch script file to remove java-pkg_clean
+
+COMMIT_MSG="Removed empty IUSE"
+TREE="/usr/portage/local/os-xtoo"
+
+cd ${TREE} || exit 1
+
+PKGS=($( grep -ls 'IUSE=""' */*/*ebuild ))
+
+batch_cmds() {
+	echo "Removing empty IUSE -> ${ebuild}"
+	sed -i '/IUSE=""/d' ${ebuild} || return 1
+	return 0
+}
