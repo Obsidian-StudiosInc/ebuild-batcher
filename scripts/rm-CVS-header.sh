5d332267666e589290bd0189e71a0321ff95b337
William L. Thomson Jr <wlt@o-sinc.com>
2017-03-23 14:59:58 -0400

    Added batch script to rm stale CVS headers

---
+++ scripts/rm-CVS-header.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Copyright 2017 Obsidian-Studios, Inc.
+# Author William L. Thomson Jr.
+#        wlt@o-sinc.com
+#
+# Distributed under the terms of The GNU Public License v3.0 (GPLv3)
+
+# Batch script file to remove stale CVS header # $Id$
+
+COMMIT_MSG="Removed CVS header"
+TREE="/usr/portage/local/os-xtoo"
+PATTERN="\\\$Id\\\$"
+
+cd ${TREE} || exit 1
+
+PKGS=($( grep -ls "${PATTERN}" */*/*ebuild ))
+
+batch_cmds() {
+	echo "${COMMIT_MSG} -> ${ebuild}"
+	sed -i "/${PATTERN}/d" ${ebuild} || return 1
+	return 0
+}
