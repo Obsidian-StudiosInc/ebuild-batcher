47f6ed7949f438e6eef369d7c86cedd5f396da88
William L. Thomson Jr <wlt@o-sinc.com>
2017-03-24 13:02:29 -0400

    Additional modifications, made general not bound to SRC_URI

--- scripts/http-to-https.sh
+++ scripts/http-to-https.sh
@@ -5,17 +5,19 @@
 #
 # Distributed under the terms of The GNU Public License v3.0 (GPLv3)
 
-# Batch script file to switch SRC_URI from http to https
+# Batch script file to switch from http to https
 
-COMMIT_MSG="SRC_URI http -> https"
+COMMIT_MSG="Switched http -> https"
 TREE="/usr/portage/local/os-xtoo"
-PATTERN="SRC_URI\=\"http\:"
+PATTERN="\=\"http\:"
 
 cd ${TREE} || exit 1
 
 PKGS=($( grep -ls "${PATTERN}" */*/*ebuild ))
 
 batch_cmds() {
+	# symlink ebuild skip, error for no change
+	[[ -L ${ebuild} ]] && return 1
 	echo "${COMMIT_MSG} -> ${ebuild}"
 	sed -i -e "s|${PATTERN}|${PATTERN/p/ps}|" ${ebuild} \
 		|| return 1
