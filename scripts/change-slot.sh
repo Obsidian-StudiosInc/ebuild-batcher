ad6c4a954181405d986b5f3f61d9ff7345020194
William L. Thomson Jr <wlt@o-sinc.com>
2017-03-25 16:51:54 -0400

    Script to change slot from variable to $(get_major_version) or other

---
+++ scripts/change-slot.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Copyright 2016 Obsidian-Studios, Inc.
+# Author William L. Thomson Jr.
+#        wlt@o-sinc.com
+#
+# Distributed under the terms of The GNU Public License v3.0 (GPLv3)
+
+# Batch script file to change slot
+
+COMMIT_MSG="Changed hardcoded slot"
+TREE="/usr/portage/local/os-xtoo"
+PATTERN="SLOT\=\"3\""
+
+cd ${TREE} || exit 1
+
+PKGS=($( grep -ls "${PATTERN}" */*/*ebuild ))
+
+batch_cmds() {
+	echo "${MSG} -> ${ebuild}"
+	sed -i -e "s|${PATTERN}|SLOT=\"$\(get_major_version\)\"|" ${ebuild} \
+		|| return 1
+	return 0
+}
